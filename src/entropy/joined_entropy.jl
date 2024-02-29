0f616a542ca9d4e1179adade5b5fffdd457775ae
MartTave <martin.tavernier256@gmail.com>
2024-02-29 10:16:46 +0100

    Ehe ?

--- src/entropy/joined_entropy.jl
+++ src/entropy/joined_entropy.jl
@@ -4,7 +4,7 @@ function getJoinedEntropy(data)
     ent_tot = 0
     for i in data
         for j in i
-            ent_tot -= j * log2(j)
+            ent_tot -= j * log(2, j)
         end
     end
     return ent_tot
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,27 @@ using Test
     data1 = [0.4, 0.3, 0.2, 0.1]
     data2 = [0.5, 0.5]
     @test SignInfLib.getEntropy(frequency) == 3.99381318311237
-    @test SignInfLib.getEntropy(data1) == 1.8464393446710157
+    @test SignInfLib.getEntropy(data1) == 1.8464393446710154
     @test SignInfLib.getEntropy(data2) == 1.0
+end
+
+@testset "General" begin
+    # Letter frequency in french
+    test1 = [5,5]
+    test2 = [4,4,2]
+    test3 = [
+        [5, 5],
+        [5, 5]
+    ]
+    test4 = [
+        [2, 2],
+        [4, 4],
+        [8, 8]
+    ]
+    res_1 = SignInfLib.convertToProb(test1)
+    res_2 = SignInfLib.convertToProb(test2)
+    res_3 = SignInfLib.convertToProb2d(test3)
+    res_4 = SignInfLib.convertToProb2d(test4)
+    @test res_1[1] == 0.5
+    @test res_3[1] == 0.25
 end
